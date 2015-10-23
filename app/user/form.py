0130f608b710af0e441a21add605c3dc73c8659f
AdHy Ngurajeka <ngurajeka.92@gmail.com>
2015-10-23 07:55:54 +0700

    Replace Login form with UserLogin Form with validation

--- app/user/form.py
+++ app/user/form.py
@@ -2,7 +2,22 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-      username = TextField('Username', validators=[DataRequired()])
-      password = PasswordField('Password', validators=[DataRequired()])
- 
+class UserLoginForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+    def validate_login(self):
+        user = self.get_user()
+
+        if user is None:
+            # raise validators.ValidationError('Invalid User')
+            return False
+
+        if not check_password_hash(user.password, self.password.data):
+            # raise validators.ValidationError('Invalid Password')
+            return False
+
+        return user
+
+    def get_user(self):
+        return db.session.query(User).filter_by(username=self.username.data).first()
