4040f48274c64c5a8aec3e424898b9d3e47a6414
AdHy Ngurajeka <ngurajeka.92@gmail.com>
2015-10-23 07:56:28 +0700

    Fixing Models, add more method, and changing method to generate password hash

--- app/user/models.py
+++ app/user/models.py
@@ -1,23 +1,43 @@
-from app.core.db import db, bcrypt
+import datetime
+from app.core.db import db
+from werkzeug.security import generate_password_hash
 
-class users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
     email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
-    password = db.Column(db.String(50))
+    created = db.Column(db.DateTime())
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return self.active
+
+    def get_fullname(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def is_anonymous(self):
+        return False
 
     def __init__(self, username, email, password, first_name, last_name):
         self.username = username
         self.email = email
-        self.password = bcrypt.generate.password.hash(password)
+        self.password = generate_password_hash(password)
         self.first_name = first_name
         self.last_name = last_name
+        if self.id is None:
+            self.created = datetime.datetime.utcnow()
 
     def __repr__(self):
-        return '<users {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
 
     @property
     def full_name(self):
-        return unicode(self.first_name + " " + self.last_name)
+        return self.get_fullname()
