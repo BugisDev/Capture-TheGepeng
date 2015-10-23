c426766d94c6d9c11ea120ba3cfa3f3dc5fdd1ff
AdHy Ngurajeka <ngurajeka.92@gmail.com>
2015-10-23 09:20:57 +0700

    Importing forgotten modul

--- app/user/form.py
+++ app/user/form.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import validators
+from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired
 
 class UserLoginForm(Form):
