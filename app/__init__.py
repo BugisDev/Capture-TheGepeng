5dcd1127074e8fc0ec875b8b2049d15a29e52f07
AdHy Ngurajeka <ngurajeka.92@gmail.com>
2015-10-23 09:45:27 +0700

    Initialize login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.core.helper import create_app
 from app.core.db import db
 from app.user.views import user_views
+from app.user.loginmanager import login_manager
 
 # Development Config
 config = 'config.dev'
@@ -9,6 +10,7 @@ config = 'config.dev'
 
 app = create_app(config)
 db.init_app(app)
+login_manager.init_app(app)
 
 # register blueprint
 app.register_blueprint(user_views)
